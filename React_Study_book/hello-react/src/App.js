ec479890744864c206abe75a2bb6203762bb658c
HongEunbeen <37995685+HongEunbeen@users.noreply.github.com>
2020-12-27 00:56:02 +0900

    props 기본 공부
    props 기본 공부

---
+++ React_Study_book/hello-react/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+import MyComponent from './MyComponent';
+
+
+const App = () => {
+  return <MyComponent name='React' favoriteNumber={1}>React</MyComponent>;
+};
+
+export default App;
