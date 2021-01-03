1bee326571ce52517e638e8c3d3784a6bf31ab95
HongEunbeen <37995685+HongEunbeen@users.noreply.github.com>
2021-01-03 17:38:36 +0900

    8.3 useReducer

--- React_Study_book/hooks-tutorial/src/Counter.js
+++ React_Study_book/hooks-tutorial/src/Counter.js
@@ -1,17 +1,33 @@
-import React , { useState } from 'react';//useSatae 가져옴
+import React , { useReducer } from 'react';//useSatae 가져옴
+
+
+function reducer(state, action){
+
+    //action.type에 따라 다른 작업 수행
+    switch(action.type) {
+        case 'INCREMENT' : 
+            return {value : state.value + 1};
+        case 'DECREMENT' : 
+            return {value : state.value - 1};
+        default :
+            //아무것도 해당되지 않을 때 기존 상태 변환
+            return state;
+    }
+}
 
 const Counter = () => {
 
-    const [value , setValue ] = useState(0);
-    //useState(기본값)
-    //useState > return 배열 > 첫번째 원소 : 상태 값 , 두번째 원소 : 상태 설정 함수(파라미터를 넣어서 호출 시 전달받은 파라미터로 값이 바뀐다음 컴포넌트 정상적 리렌더링)
-
+    const [state, dispatch] = useReducer(reducer , {value : 0});
+    //useReducer(리듀서 함수 , 해당 리듀서의 기본값)
+    //useReducer return > state 값 : 현재 가리키고 있는 상태, dispatch 함수 : 액션을 발생시키는 함수
+    //dispatch(action) > 함수 안에 파라미터로 액션 값을 넣어주면 리듀서 함수 호출 구조
+    
     return(
         <div>
             <p>
-                현재 카운터 값은 <b>{value}</b>입니다.
-                <button onClick={() => setValue(value + 1)}>+1</button>
-                <button onClick={() => setValue(value - 1)}>-1</button>
+                현재 카운터 값은 <b>{state.value }</b>입니다.
+                <button onClick={() => dispatch({type : 'INCREMENT'})}>+1</button>
+                <button onClick={() => dispatch({type : 'DECREMENT'})}>-1</button>
             </p>
         </div>
     );
--- React_Study_book/hooks-tutorial/src/Info.js
+++ React_Study_book/hooks-tutorial/src/Info.js
@@ -1,46 +1,35 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useReducer} from 'react';
+
+function reducer(state, action) {
+    return{
+        ...state,
+        [action.name] : action.value
+    };
+}
 
 const Info = () =>{
 
-    const [name, setName] = useState('');
-    const [nickname, setNickname] = useState('');
+    // const [name, setName] = useState('');
+    // const [nickname, setNickname] = useState('');
 
-    useEffect(() => {
-        console.log('effect');
-        console.log(name);
-        return() => {
-            console.log('cleanup');
-            console.log(name);
-        };
-    },[name]);
-    //두번째 매개변수에 배열을 넣으면 마운트, 언마운트 시에만 작동
-    //두번째 매개변수에 배열에 검사 값을 넣으면 그 값만 마운트 업데이트 언마운트 시 작동
+    const [state, dispatch] = useReducer(reducer,{
+        name : '',
+        nickname : ''
+    })
 
-    /*
-    useEffect(() => {
-        console.log(name);
-    },[name]);
-    //배열 안에 useState 통해 관리하고 있는 상태 넣어도 되고, props로 전달받은 값 넣어도 된다
+    const {name, nickname} = state;
+
+    const onChange = e =>{
+        dispatch(e.target);
     
-    useEffect(() => {
-        console.log('렌더링이 완료되었습니다!');
-    },[]);
-    */
-
-    const onChangeName = e =>{
-        setName(e.target.value);
-    }
-
-    const onChangeNickname = e =>{
-        setNickname(e.target.value);
     }
 
 
     return(
         <div>
             <div>
-                <input value={name} onChange={onChangeName}/>
-                <input value={nickname} onChange={onChangeNickname}/>
+                <input value={name} onChange={onChange}/>
+                <input value={nickname} onChange={onChange}/>
             </div>
             <div>
                 <div>
