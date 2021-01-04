78de7c2b0aeae65decf82afd107e44f2212df6d6
HongEunbeen <37995685+HongEunbeen@users.noreply.github.com>
2021-01-04 21:57:48 +0900

    10장 일정관리 웹 애플리케이션 만들기 finish

--- React_Study_book/todo-app/src/App.js
+++ React_Study_book/todo-app/src/App.js
@@ -23,6 +23,31 @@ const App =()=>{
       checked : false
     },
   ]);
+
+  const onRemove = useCallback(
+    id => {
+      setTodos(todos.filter(todo => todo.id !== id));
+      //배열의 불변성을 지키면서 배열 원소를 제거해야 된다
+      //filter > 기존의 배열은 그대로 둔 상태에서 특정 조건 만족 원소만 따로 추출해 새로운 배열 만들어 준다
+      //조건의 활인해 주는 함수(boolean 값) : 파라미터 > true 만 새로운 배열에 존재
+    },
+    [todos]
+  );
+
+  const onToggle = useCallback(
+    id => {
+      setTodos(
+        //배열 내장 함수 map을 사용해 특정 id 가지고 있는 객체의 checked 값을 반전시켜 준다.
+        //배열의 분별성 유지하면서 특정 배열 원소 업데이트
+        //배열의 분별성 유지 추출(삭제) : filter / 배열의 분별성 유지 업데이트 : map
+        todos.map(todo =>
+          todo.id === id ? { ... todo , checked : !todo.checked} : todo
+          )
+      );
+    },
+    [todos]
+  );
+
   //고윳값으로 사용될 id > 렌더링 필요 x 으로 useState가 아닌 useRef 사용해 컴포넌트에서 사용할 변수 생성
   //ref를 사용하여 변수 담기
   const nextId = useRef(4);
@@ -44,7 +69,7 @@ const App =()=>{
   return (
     <TodoTemplate>
         <TodoInsert onInsert={onInsert}/>
-        <TodoList todos={todos}/>
+        <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
     </TodoTemplate>
   );
 }
