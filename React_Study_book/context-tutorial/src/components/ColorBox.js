8a160d42a8f2fa8db31242f5d0aa0455fcf8ebca
HongEunbeen <37995685+HongEunbeen@users.noreply.github.com>
2021-01-27 23:49:33 +0900

    16장 start

--- React_Study_book/context-tutorial/src/components/ColorBox.js
+++ React_Study_book/context-tutorial/src/components/ColorBox.js
@@ -1,28 +1,29 @@
-import React from 'react';
-import {ColorConsumer} from '../contexts/color';
+import React,{useContext} from 'react';
+import {ColorContext} from '../contexts/color';
 
     // {/*패턴을 function as a child 혹은 Render Props */}
     // {/*ColorContext안에 들어있는 Consumer 컴포넌트 통해 색상 조회(props x) */}
 
 const ColorBox = () => {
+
+  const {state} = useContext(ColorContext);
   return (
-    <ColorConsumer>
-        {({state}) => (
-            <>
-            <div
-                style={
-                    {width:'64px', height:'64px', background: state.color}
-                }
-            />
-            <div
-                style={
-                    {width:'64px', height:'64px', background: state.subcolor}
-                }
-            />
-            </>
-            
-        )}
-    </ColorConsumer>
+    <>
+        <div
+            style={{
+                width:'64px',
+                height:'64px',
+                background: state.color
+            }}
+        />
+        <div
+            style={{
+                width : '32px',
+                height : '32px',
+                background : state.subcolor
+            }}
+        />
+    </>
   );
 };
 
--- React_Study_book/context-tutorial/src/contexts/color.js
+++ React_Study_book/context-tutorial/src/contexts/color.js
@@ -32,3 +32,12 @@ const {Consumer : ColorConsumer} = ColorContext;
 export { ColorProvider, ColorConsumer};
 
 export default ColorContext;
+
+
+
+const listener = () => {
+    console.log('상태가 업데이트됨');
+}
+const unsubscribe = store.subscribe(listener);
+
+unsubscribe();//추후 구독을 비활성화할 때 함수를 호출
