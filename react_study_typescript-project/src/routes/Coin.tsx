462c67ec070d0674521fad7f6932011a6de3604b
HongEunbeen <vvvv980@gmail.com>
2022-02-09 01:00:39 +0900

    nested router & location 버그 수정

--- react_study_typescript-project/src/routes/Coin.tsx
+++ react_study_typescript-project/src/routes/Coin.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Outlet, Route , Routes, useLocation, useParams } from "react-router-dom";
 import styled from 'styled-components';
+import Chart from "./Chart";
 import { ICoin } from "./Coins";
+import Price from "./Price";
 
 const Container = styled.div`
     padding:0px 20px;
@@ -25,25 +27,148 @@ const Loader = styled.span`
     display : block;
 `
 
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
 interface IRouteParams {
     coinId:string;
 }
 
-interface IRouteStat extends Pick<ICoin, "name"> { }
+interface IRouteStat {
+    name?:string;
+}
+
+interface IInfoData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+    contract: string;
+    platform: string;
+    description: string;
+    message: string;
+    open_source: boolean;
+    hardware_wallet: boolean;
+    first_data_at: string;
+    last_data_at: string;
+}
+
+
+interface IPriceData {
+    d : string;
+    name : string;
+    symbol : string;
+    rank : number;
+    circulating_supply : number;
+    total_supply : number;
+    max_supply : number;
+    beta_value : number;
+    first_data_at : string;
+    last_updated : string;
+    quotes:{
+        USD :{
+            ath_date: string;
+            ath_price:number;
+            market_cap:number;
+            percent_change_1h:number;
+            percent_change_1y:number;
+            percent_change_6h:number;
+            percent_change_7d:number;
+            percent_change_12h:number;
+            percent_change_15m:number;
+            percent_change_24h:number;
+            percent_change_30d:number;
+            percent_change_30m:number;
+            percent_from_price_ath:number;
+            price:number;
+            volume_24h:number;
+            volume_24h_change_24h:number;
+        }
+    }
+}
 
 function Coin() {
     const {coinId} = useParams() as unknown as IRouteParams;
-    const {name} = useLocation().state as IRouteStat;
+    const local = useLocation();
+    const {name} = local.state === null ? {name:null} : local.state as IRouteStat;
+
     const [loading, setLoding] = useState(true);
+    const [info, setInfo] = useState<IInfoData>();
+    const [priceInfo, setPriceInfo] = useState<IPriceData>();
+
+    useEffect(() => {
+        (async() => {
+            const infoData = await (
+                await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+            ).json();
+            const priceData = await (
+                await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+            ).json();
+
+            setInfo(infoData);
+            setPriceInfo(priceData);
+            setLoding(false);
+        })();
+    }, [coinId]);
 
     return(
         <Container>
             <Header>
-                <Title>{name?? "Loading..."}</Title>
+                <Title>{"Loading..."}</Title>
             </Header>
             {loading ? 
             <Loader>로딩로딩</Loader>
-            : null
+            : 
+            <>
+            <Overview>
+                <OverviewItem>
+                    <span>Rank:</span>
+                    <span>{info?.rank}</span>
+                </OverviewItem>
+                <OverviewItem>
+                    <span>Symbol:</span>
+                    <span>${info?.symbol}</span>
+                </OverviewItem>
+                <OverviewItem>
+                    <span>Open Source:</span>
+                    <span>{info?.open_source ? "Yes" : "No"}</span>
+                </OverviewItem>
+            </Overview>
+            <Description>{info?.description}</Description>
+            <Overview>
+                <OverviewItem>
+                    <span>Total Suply:</span>
+                    <span>{priceInfo?.total_supply}</span>
+                </OverviewItem>
+                <OverviewItem>
+                    <span>Max Supply:</span>
+                    <span>{priceInfo?.max_supply}</span>
+                </OverviewItem>
+            </Overview>
+            <Outlet />
+            </>
             }
         </Container>
     )
